0a657c6d9c6a00167af9272610de11b5e5cfe5ca
佛系小哥哥 <zhenqionghuang@creditease.cn>
2019-06-20 14:01:02 +0800

    add new files

---
+++ src/main/java/com/holylala/springbootstu/SpringbootstuApplication.java
@@ -0,0 +1,13 @@
+package com.holylala.springbootstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstuApplication.class, args);
+    }
+
+}
